9ee49ecf207b88ec1ddbebac24ad88f5085dea7b
Vincent Blansaer <mephisto0666@gmail.com>
2023-02-12 20:59:23 -0600

    Update TuckboxGenerator_KingdomDeathMonster.jsx

--- TuckboxGenerator_KingdomDeathMonster.jsx
+++ TuckboxGenerator_KingdomDeathMonster.jsx
@@ -1,4 +1,4 @@
-﻿#include 'TuckboxGenerator.jsx'
+#include 'TuckboxGenerator.jsx'
 
 var kingdomDeathMonsterTuckBoxImage = "D:\\Temp\\Tuckboxes\\KDM\\kdm.png";
 var kingdomDeathMonsterLogoImage = "D:\\Temp\\Tuckboxes\\KDM\\logo.png";
@@ -18,6 +18,382 @@ var kingdomDeathMonsterWhiteLionTuckBox = new TuckBox(
 	ffgRegularSleevedCard, 
 	"Kingdom Death: Monster White Lion Cards" );
 	
+// Kingdom Death: Monster: Dung Beetle Knight Cards
+var kingdomDeathMonsterDungBeetleTuckBox = new TuckBox( 	
+	84, 
+	[new BoxText("Dung Beetle Knight Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\dungbeetle_basic.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\dungbeetle_ai.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\dungbeetle_hitlocation.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\dungbeetle_hunt.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\dungbeetle_resource.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Dung Beetle Knight v 1.0"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Dung Beetle Knight Cards" );
+	
+// Kingdom Death: Monster: Screaming Antelope Cards
+var kingdomDeathMonsterAntelopeTuckBox = new TuckBox( 	
+	78, 
+	[new BoxText("Screaming Antelope Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\antelope_basic.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\antelope_ai.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\antelope_hitlocation.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\antelope_hunt.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\antelope_resource.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Screaming Antelope v 1.0"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Screaming Antelope Cards" );
+	
+// Kingdom Death: Monster: Phoenix Cards
+var kingdomDeathMonsterPhoenixTuckBox = new TuckBox( 	
+	87, 
+	[new BoxText("Phoenix Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\phoenix_basic.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\phoenix_ai.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\phoenix_hitlocation.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\phoenix_hunt.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\phoenix_resource.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Phoenix v 1.0"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Phoenix Cards" );
+	
+// Kingdom Death: Monster: Butcher Cards
+var kingdomDeathMonsterButcherTuckBox = new TuckBox( 	
+	46, 
+	[new BoxText("Butcher Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\butcher_basic.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\butcher_ai.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\butcher_hitlocation.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Butcher v 1.0"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Butcher Cards" );
+	
+// Kingdom Death: Monster: Manhunter Cards
+var kingdomDeathMonsterManhunterTuckBox = new TuckBox( 	
+	53, 
+	[new BoxText("Manhunter Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\manhunter_basic.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\manhunter_ai.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\manhunter_hitlocation.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Manhunter v 1.0"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Manhunter Cards" );
+	
+// Kingdom Death: Monster: Slenderman Cards
+var kingdomDeathMonsterSlendermanTuckBox = new TuckBox( 	
+	52, 
+	[new BoxText("Slenderman Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\slenderman_basic.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\slenderman_extra.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\slenderman_ai.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\slenderman_hitlocation.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Slenderman v 1.0"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Slenderman Cards" );
+	
+// Kingdom Death: Monster: Dragon King Cards
+var kingdomDeathMonsterDragonKingTuckBox = new TuckBox( 	
+	76, 
+	[new BoxText("Dragon King Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\dragonking_basic.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\dragonking_ai.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\dragonking_hitlocation.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\dragonking_hunt.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\dragonking_resource.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Dragon King v 1.0"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Dragon King Cards" );
+	
+// Kingdom Death: Monster: The Tyrant Cards
+var kingdomDeathMonsterTheTyrantTuckBox = new TuckBox( 	
+	49, 
+	[new BoxText("The Tyrant Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\tyrant_basic.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\tyrant_ai.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\tyrant_hitlocation.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"The Tyrant v 1.0"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster The Tyrant Cards" );
+	
+// Kingdom Death: Monster: The Gorm Cards
+var kingdomDeathMonsterTheGormTuckBox = new TuckBox( 	
+	74, 
+	[new BoxText("The Gorm Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\gorm_basic.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\gorm_ai.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\gorm_hitlocation.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\gorm_hunt.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\gorm_resource.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"The Gorm v 1.0"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster The Gorm Cards" );
+	
+// Kingdom Death: Monster: Sunstalker Cards
+var kingdomDeathMonsterSunstalkerTuckBox = new TuckBox( 	
+	86, 
+	[new BoxText("Sunstalker Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\sunstalker_basic.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\sunstalker_ai.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\sunstalker_hitlocation.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\sunstalker_hunt.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\sunstalker_resource.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Sunstalker v 1.0"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Sunstalker Cards" );
+	
+// Kingdom Death: Monster: The Hand Cards
+var kingdomDeathMonsterTheHandTuckBox = new TuckBox( 	
+	41, 
+	[new BoxText("The Hand Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\hand_basic.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\hand_ai.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\hand_hitlocation.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"The Hand v 1.0"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster The Hand Cards" );
+	
+// Kingdom Death: Monster: King's Man Cards
+var kingdomDeathMonsterKingsManTuckBox = new TuckBox( 	
+	52, 
+	[new BoxText("King's Man Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\kingsman_basic.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\kingsman_ai.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\kingsman_hitlocation.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"King's Man v 1.0"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster King's Man Cards" );
+	
+// Kingdom Death: Monster: Watcher Cards
+var kingdomDeathMonsterWatcherTuckBox = new TuckBox( 	
+	53, 
+	[new BoxText("Watcher Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\watcher_basic.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\watcher_ai.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\watcher_hitlocation.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Watcher v 1.0"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Watcher Cards" );
+	
+// Kingdom Death: Monster: Gold Smoke Knight Cards
+var kingdomDeathMonsterGoldSmokeKnightTuckBox = new TuckBox( 	
+	42, 
+	[new BoxText("Gold Smoke Knight Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\goldsmokeknight_basic.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\goldsmokeknight_ai.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\goldsmokeknight_hitlocation.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Gold Smoke Knight v 1.0"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Gold Smoke Knight Cards" );
+	
+// Kingdom Death: Monster: Secret Fighting Arts Cards
+var kingdomDeathMonsterSecretFightingArtsTuckBox = new TuckBox( 	
+	21, 
+	[new BoxText("Secret Fighting Arts Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\secretfightingarts.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\secretfightingarts.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\secretfightingarts.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Secret Fighting Arts v 1.0", "Core + Gorm + DBK + DK + SS + MH + SM"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Secret Fighting Arts Cards" );
+	
+// Kingdom Death: Monster: Fighting Arts Cards
+var kingdomDeathMonsterFightingArtsTuckBox = new TuckBox( 	
+	42, 
+	[new BoxText("Fighting Arts Cards", 16), new BoxText("Tactics Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\tactics.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\tactics.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\fightingarts.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\fightingarts.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Fighting Arts + Tactics v 1.0", "Core + Gorm + DBK + DK + SS + MH + SM"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Fighting Arts Cards" );
+	
+// Kingdom Death: Monster: Armor And Proficiency Cards
+var kingdomDeathMonsterArmorProficiencyTuckBox = new TuckBox( 	
+	24, 
+	[new BoxText("Armor Set Cards", 16), new BoxText("Weapon Proficiency Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\armor1.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\armor2.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\proficiency1.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\proficiency2.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Armor Sets + Weapon Proficiencies v 1.0", "Core + Gorm + DBK + DK + SS + MH + SM"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Armor And Proficiency Cards" );
+	
+// Kingdom Death: Monster: Innovations Cards
+var kingdomDeathMonsterInnovationsTuckBox = new TuckBox( 	
+	64, 
+	[new BoxText("Innovations Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\innovations.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\innovations.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\innovations.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Innovations v 1.0", "Core + Gorm + DBK + DK + SS + MH + SM"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Innovations Cards" );
+	
+// Kingdom Death: Monster: Strange Resources Cards
+var kingdomDeathMonsterStrangeResourcesTuckBox = new TuckBox( 	
+	56, 
+	[new BoxText("Strange Resources Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\strangeresource.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\strangeresource.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\strangeresource.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Strange Resources v 1.0", "Core + Gorm + DBK + DK + SS + MH + SM"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Strange Resources Cards" );
+	
+// Kingdom Death: Monster: Basic Resources And Vermin Cards
+var kingdomDeathMonsterBasicResourcesVerminTuckBox = new TuckBox( 	
+	32, 
+	[new BoxText("Basic Resources Cards", 16), new BoxText("Vermin Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\vermin.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\vermin.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\basicresource.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\basicresource.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Basic Resources + Vermin v 1.0", "Core + Gorm + DBK + DK + SS + MH + SM"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Basic Resources And Vermin Cards" );
+	
+// Kingdom Death: Monster: Disorders Cards
+var kingdomDeathMonsterDisordersTuckBox = new TuckBox( 	
+	36, 
+	[new BoxText("Disorders Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\disorders.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\disorders.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\disorders.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Disorders v 1.0", "Core + Gorm + DBK + DK + SS + MH + SM"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Disorders Cards" );
+	
+// Kingdom Death: Monster: Universal Cards
+var kingdomDeathMonsterUniversalTuckBox = new TuckBox( 	
+	18, 
+	[new BoxText("Universal Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\universal.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\universal.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\universal.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Universal v 1.0", "Core + Gorm + DBK + DK + SS + MH + SM"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Universal Cards" );
+	
+// Kingdom Death: Monster: Terrain Cards
+var kingdomDeathMonsterTerrainTuckBox = new TuckBox( 	
+	26, 
+	[new BoxText("Terrain Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\terrain.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\terrain.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\terrain.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Terrain v 1.0", "Core + Gorm + DBK + DK + SS + MH + SM"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Terrain Cards" );
+	
+// Kingdom Death: Monster: Hunt Events Cards
+var kingdomDeathMonsterHuntEventsTuckBox = new TuckBox( 	
+	15, 
+	[new BoxText("Hunt Events Cards", 16)], 
+	kingdomDeathMonsterTuckBoxImage, 
+	["D:\\Temp\\Tuckboxes\\KDM\\specialhunt.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\hunt.png",
+	"D:\\Temp\\Tuckboxes\\KDM\\hunt.png"], 
+	kingdomDeathMonsterLogoImage, 
+	["Horizontal Tuckboxes: Kingdom Death: Monster", 
+	"Hunt Events v 1.0", "Core + Gorm + DBK + DK + SS + MH + SM"], 
+	ffgRegularSleevedCard, 
+	"Kingdom Death: Monster Hunt Events Cards" );
+	
 var tuckboxDrawer = new TuckboxDrawer( dpi, documentSize.LETTER );
 tuckboxDrawer.drawTuckbox( kingdomDeathMonsterWhiteLionTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterDungBeetleTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterAntelopeTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterPhoenixTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterButcherTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterManhunterTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterSlendermanTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterDragonKingTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterTheTyrantTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterTheGormTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterSunstalkerTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterTheHandTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterKingsManTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterWatcherTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterGoldSmokeKnightTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterSecretFightingArtsTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterFightingArtsTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterArmorProficiencyTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterInnovationsTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterStrangeResourcesTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterBasicResourcesVerminTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterDisordersTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterUniversalTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterTerrainTuckBox );
+tuckboxDrawer.drawTuckbox( kingdomDeathMonsterHuntEventsTuckBox );
 tuckboxDrawer = null;
